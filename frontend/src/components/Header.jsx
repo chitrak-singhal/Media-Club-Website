10103244d005c9fd73583b1e960209ea85d1aebc
chitrak-singhal <chitrak.singhal.eee21@itbhu.ac.in>
2024-05-24 03:45:06 +0530

    Added transition to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
 import { images } from '../constants'
 
+const navItemsInfo = [
+    {name:"Home"},
+    {name:"Articles"},
+    {name:"Interviews"},
+    {name:"Surveys"},
+]
+
+const NavItem = ({name}) =>{
+    return (
+        <li className='relative group'>
+            <a href='' className='px-2 py-2 group-hover:text-underline'>{name}</a>
+            <span className='text-white absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[95%] opacity-0 group-hover:opacity-100'>/</span>
+        </li>
+    )
+}
+
 const Header = () => {
   return (
     <section>
-        <header className='container mx-auto px-5 flex justify-center py-4 bg-black items-center'>
+        <header className='container mx-auto px-5 flex justify-center py-4 bg-black items-center font-semibold'>
             <div className='bg-black w-1/3 flex'>
                 <img src={images.Logo} alt="logo" className='h-10 mr-5  items-center'/>
                 <div className='flex items-center'>
                 <p className='text-white align-middle'>The Media Club IIT BHU</p>
                 </div>
             </div>
-            <div className='bg-black flex gap-x-9 w-1/3 justify-center'>
+            <div className='bg-black flex gap-x-9 w-1/3 justify-center font-medium'>
                 <ul className='flex gap-x-5 text-white'>
-                    <li>
-                        <a href=''>Home</a>
-                    </li>
-                    <li>
-                        <a href=''>Articles</a>
-                    </li>
-                    <li>
-                        <a href=''>Interviews</a>
-                    </li>
-                    <li>
-                        <a href=''>Surveys</a>
-                    </li>
+                    {navItemsInfo.map(item =>(
+                        <NavItem key={item.name} name={item.name}/>
+                    ))}
                 </ul>
             </div>
             <div className='w-1/3 flex justify-end'>
